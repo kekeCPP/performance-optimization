7b56e497362a19df9217d7bab90e6eda4b8b3fc0
Niklas Johansson <rapsabamse1@gmail.com>
2023-10-11 17:08:50 +0100

    thread start

--- filters/filters/filters.cpp
+++ filters/filters/filters.cpp
@@ -246,7 +246,7 @@ Matrix blur_par(Matrix &dst, const int radius, const int MAX_THREADS)
 
 
 void *threadFunc(void * thread_arg){
-    int which = * (int *)thread_arg
+    int which = * (int *)thread_arg;
     std::cout << "Thread" << which << "is working\n";
 
     pthread_exit(NULL);
