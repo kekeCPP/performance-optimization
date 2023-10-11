f5423dd2479db8891e9a24307789d23c8e0d3b2b
Niklas Johansson <rapsabamse1@gmail.com>
2023-10-11 14:37:07 +0100

    2

--- filters/filters/blur.cpp
+++ filters/filters/blur.cpp
@@ -21,7 +21,6 @@ int main(int argc, char const* argv[])
     auto m { reader(argv[2]) };
     auto radius { static_cast<unsigned>(std::stoul(argv[1])) };
 
-    auto blurred { Filter::blur(m, radius, std::stoul(argv[4])) };
     Filter::blur(m, radius, std::stoul(argv[4]));
     writer(blurred, argv[3]);
 
